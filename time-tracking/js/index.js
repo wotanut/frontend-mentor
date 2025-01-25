7da9a29c0ef6c02cb87e5cd223d9b5f85d1c2ab8
wotanut <sam@sambot.dev>
2025-01-25 09:21:05 +0000

    change URL to prod URL

--- time-tracking/js/index.js
+++ time-tracking/js/index.js
@@ -8,7 +8,11 @@ var details;
 function loadJSON(callback) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open("GET", "../data.json", true);
+  xobj.open(
+    "GET",
+    "https://playgrounds.sambot.dev/time-tracking/data.json",
+    true
+  );
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
